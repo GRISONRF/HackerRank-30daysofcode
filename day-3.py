94632806a32fb878a2370b2a4704aea919e68527
GRISONRF <grisonrf@gmail.com>
2022-08-11 10:42:31 -0600

    day 3

--- day-3.py
+++ day-3.py
@@ -13,15 +13,16 @@
 
 
 if __name__ == '__main__':
-    N = int(input().strip())
+    n = int(input().strip())
     
-# if n % 2 != 0 :
-#     print ('Weird')
-# elif n % 2 == 0:
-#     if n >= 2 and n <=5:
-#         print('Nor Weird')
-#     elif n >= 6 and n <= 20:
-#         print('Weird')
-#     elif n > 21:
-#         print('Not Weird')
-    print("Weird" if ((N%2!=0) or (N%2==0 and N in range(6,21))) else "Not Weird")
+if n % 2 != 0 :
+    print ('Weird')
+    
+    
+elif n % 2 == 0:
+    if n >= 2 and n <=5:
+        print('Not Weird')
+    elif n >= 6 and n <= 20:
+        print('Weird')
+    elif n >= 21:
+        print('Not Weird')
