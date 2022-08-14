7fc8e40d4e4fc46d4f4ba27e919536f9d964cd2f
GRISONRF <grisonrf@gmail.com>
2022-08-13 22:08:58 -0600

    day 4

---
+++ day-4.py
@@ -0,0 +1,4 @@
+# Objective
+# In this challenge, we're going to learn about the difference between a class and an instance; because this is an Object Oriented concept, it's only enabled in certain languages.
+
+
